e7069bd8ebba3c5a079f176ead51346b5dc6c83f
Misch@ <Michaela.pupikova22@gmail.com>
2022-04-23 21:30:00 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import { Intro } from '../Intro/Intro';
+import { SimonBoard } from '../SimonBoard/SimonBoard';
+
+import './../../styles/normalize.css';
+import '../../styles/app.css';
+
+export const App = () => (
+	<>
+		<Intro />
+		<SimonBoard />
+	</>
+);
