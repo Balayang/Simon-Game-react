f634277c3195b7f649fe1b93d09e7bec429669be
martinfranek <martin.ffranek@gmail.com>
2022-04-24 15:59:58 +0200

    fix props for ColorCard component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,55 @@
-import React, { useEffect } from 'react';
-import { ColorCard } from '../ColorCard/ColorCard';
-import classNames from 'classnames/bind';
+import React from "react";
+import { ColorCard } from "../ColorCard/ColorCard";
 
-import './../../styles/normalize.css';
-import '../../styles/app.css';
-
-
-
+import "./../../styles/normalize.css";
+import "../../styles/app.css";
 
 export const App = () => {
-	const [hidden, setHidden] = React.useState(undefined);
-	const [score, setScore] = React.useState(0);
+  const [hidden, setHidden] = React.useState(undefined);
+  const [score, setScore] = React.useState(0);
 
-	return (
-		<>
-			<section hidden={hidden} className="intro">
-				<h1 className="title">SIMON</h1>
-				<h2 className="sublititle">Do what Simon says!</h2>
-				<p className="instructions">
-					Follow the lights and patterns as long as you can...
-				</p>
-				<p
-					onClick={() => {
-						setHidden(true);
-					}}
-					className="btn btnStart"
-				>
-					Start
-				</p>
-			</section>
-			<section className="container">
-				<div className="gameBoard">
-					<ColorCard color='green'></ColorCard>
-					<ColorCard color="red"></ColorCard>
-					<ColorCard color="blue"></ColorCard>
-					<ColorCard color="yellow"></ColorCard>
-				</div>
-				<section className="scoreBoard">
-					<p className="quit">Quit</p>
-					<p className="scoreCount">Count: {score}</p>
-				</section>
-			</section>
-		</>
-	);
+  // jenom placeholder (ve skutecnosti to bude state)
+  const flash = false;
+
+  const onClick = (color) => {
+    console.log(`clicked on ${color} button...`);
+  };
+
+  return (
+    <>
+      {/* section je klasicky html tag a ten sam o sobe nema hidden propu */}
+      {/* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section */}
+      <section hidden={hidden} className="intro">
+        <h1 className="title">SIMON</h1>
+        <h2 className="sublititle">Do what Simon says!</h2>
+        <p className="instructions">
+          Follow the lights and patterns as long as you can...
+        </p>
+        <p onClick={() => setHidden(true)} className="btn btnStart">
+          Start
+        </p>
+      </section>
+      <section className="container">
+        <div className="gameBoard">
+          {["green", "red", "blue", "yellow"].map((color) => (
+            <ColorCard
+              color={color}
+              flash={flash}
+              // todle je taky placeholder, takto to prida jedno skore, kdyz kliknes na jednu barvu
+              // ten druhy onClick je pro console log barvy na kterou jsi klikla
+              onClick={() => setScore((prevScore) => prevScore + 1)}
+              // onClick={() => onClick(color)}
+            />
+          ))}
+        </div>
+        <section className="scoreBoard">
+          {/* todle prijde opravit :D */}
+          <button className="btn quit" onClick={() => setHidden(false)}>
+            Quit
+          </button>
+          <p className="scoreCount">Score: {score}</p>
+        </section>
+      </section>
+    </>
+  );
 };
--- src/components/ColorCard/ColorCard.jsx
+++ src/components/ColorCard/ColorCard.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import classNames from 'classnames/bind';
+import React from "react";
+import classNames from "classnames/bind";
 
-import styles from './ColorCard.module.css';
+import styles from "./ColorCard.module.css";
 
 const cx = classNames.bind(styles);
 
-export const ColorCard = (color, onClick, flash) => {
-	return (
-		<div
-			onClick={onClick}
-			className={cx('colorCard', 'color',
-				'flash' ? 'flash' : '')}
-		></div>
-	);
-};
+export const ColorCard = ({ color, flash, onClick }) => (
+  <div onClick={onClick} className={cx(color, flash, "colorCard")} />
+);
